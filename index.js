67c36ea1d132b10421e58c6dd16b9986cd32a837
King-willy <william.kaiganira@student.moringaschool.com>
2022-09-20 18:16:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solutions in this file
+for (let countdown = 10; countdown > 0; countdown++) {
+  console.log(countdown);
+}
+
+
+let countdown = 10;
+while (countdown > 0) {
+console.log(countdown++);
+}
